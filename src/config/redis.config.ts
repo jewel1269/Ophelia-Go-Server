953e5c93468d67dd772b881be6a7b5a5cab281c4
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2026-02-01 20:43:26 +0600

    add redis url

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,17 +1,17 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: process.env.REDIS_URL || 'redis://localhost:6379',
 });
 
-redisClient.on("error", (err) => {
-  console.error("❌ Redis Error:", err);
+redisClient.on('error', (err) => {
+  console.error('❌ Redis Error:', err);
 });
 
-redisClient.on("connect", () => {
-  console.log("🚀 Redis Connected Successfully");
+redisClient.on('connect', () => {
+  console.log('🚀 Redis Connected Successfully');
 });
 
-redisClient.connect(); 
+redisClient.connect();
 
 export default redisClient;
